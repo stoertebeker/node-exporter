a9f725832fe0e65e63c9044f7b11327eaccefa19
stoertebeker <stoertebeker@kkb-clan.de>
2018-01-26 21:02:10 +0100

    Create install-node-exporter.sh

---
+++ install-node-exporter.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+wget https://github.com/prometheus/node_exporter/releases/download/v0.15.2/node_exporter-0.15.2.linux-amd64.tar.gz -O /tmp/nodes_exporter.tar.gz
+tar xvzf /tmp/nodes_exporter.tar.gz
+rm /tmp/nodes_exporter.tar.gz
+mv node_exporter-0.15.2.linux-amd64 node_exporter
+sudo ln -s ~/node_exporter/node_exporter /usr/bin/node_exporter
+screen -AmdS node_exporter node_exporter
+curl localhost:9100/metrics

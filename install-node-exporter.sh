6ab78cb59160150b51644a8ef9db116adb6acddc
stoertebeker <stoertebeker@kkb-clan.de>
2018-01-29 22:04:06 +0100

    Update install-node-exporter.sh

--- install-node-exporter.sh
+++ install-node-exporter.sh
@@ -2,7 +2,7 @@
 sudo useradd --no-create-home --shell /bin/false node_exporter
 wget https://github.com/prometheus/node_exporter/releases/download/v0.15.2/node_exporter-0.15.2.linux-amd64.tar.gz
 tar xvzf node_exporter-0.15.2.linux-amd64.tar.gz
-sudo mv node_exporter-0.15.2.linux-amd64/node_exporter /usr/local/bin
+sudo cp node_exporter-0.15.2.linux-amd64/node_exporter /usr/local/bin
 sudo chown node_exporter:node_exporter /usr/local/bin/node_exporter
 rm -rf node_exporter-0.15.2.linux-amd64*
 sudo mv node_exporter.service /etc/systemd/system/
